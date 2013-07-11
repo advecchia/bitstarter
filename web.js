875f21ee38854279efd8a02e8de119596887bf39
Alessandro Vecchia <advecchia@inf.ufrgs.br>
2013-07-11 02:58:34 +0000

    Finally correct update web.js :)

--- web.js
+++ web.js
@@ -1,12 +1,10 @@
 var fs = require('fs');
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var buf = fs.readFileSync('index.html','utf-8')
 
 app.get('/', function(request, response) {
-  var buf = fs.readFileSync('index.html',String)
-  response.send(buf.toString('utf-8'));
-  //response.send('Hello World 2! (Olá Mundo 2! in portuguese)');
+  response.send(buf);
 });
 
 var port = process.env.PORT || 5000;
